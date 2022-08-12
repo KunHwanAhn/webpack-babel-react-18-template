067b895112e9b2d9ba5b42814d975a8058c8d76e
Conan Kunhwan Ahn <kunhwon@gmail.com>
2022-08-12 15:28:05 +0900

    PropType 의존성 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
-import React from 'react';
-import './App.scss';
+import React, { useState } from 'react';
 
 import MyButton from '@/components/MyButton.jsx';
+import UserProfile from '@/components/UserProfile.jsx';
+
+import './App.scss';
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
   return (
     <div className="App">
       <h1>Hello, World!</h1>
-      <MyButton />
+      <div>
+        <MyButton
+          count={count}
+          onClick={() => handleClick()}
+        />
+      </div>
+      <div>
+        <MyButton
+          count={count}
+          onClick={() => handleClick()}
+        />
+      </div>
+      <hr />
+      <UserProfile />
     </div>
   );
 }
--- src/components/MyButton.jsx
+++ src/components/MyButton.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function MyButton() {
+export default function MyButton({ count, onClick }) {
   return (
-    <button type="button">I&apos;m a button</button>
+    <button
+      type="button"
+      onClick={onClick}
+    >
+      {`Clicked ${count} times`}
+    </button>
   );
 }
+
+MyButton.propTypes = {
+  count: PropTypes.number.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
